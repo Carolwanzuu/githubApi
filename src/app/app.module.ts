fd22e4f5cc7052b919a5fc0e34c7864dbb4048e1
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-21 14:53:13 +0300

    generated a component for repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
