e6023b85bbe61a8d7a4657a073bb036c13121f41
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-21 15:19:30 +0300

    imported the Api key to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubApiKey:'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'
 };
 
 /*
