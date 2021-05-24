092f868e0bccf705dd872dfbb3c0ff51556be3ed
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-24 04:14:27 +0300

    modified the service to check on functionality

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {UsersService} from '../users.service';
+//import {UsersService} from '../users.service';
 
 @Component({
   selector: 'app-user',
@@ -10,15 +10,15 @@ export class UserComponent implements OnInit {
 
   users:any[]=[];
 
-  constructor( private usersService:UsersService) { }
+  // constructor( private usersService:UsersService) { }
 
-  ngOnInit() {
-    this.usersService.getUsers()
-    .subscribe((response:any)=>{
-      console.log('Users',response);
-      this.users=response.object;
-    })
+  // ngOnInit() {
+  //   this.usersService.getUsers()
+  //   .subscribe((response:any)=>{
+  //     console.log('Users',response);
+  //     this.users=response.object;
+  //   })
 
-  }
+  // }
 
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {environment} from '../environments/environment';
+// import { Injectable } from '@angular/core';
+// import {HttpClient, HttpClientModule} from '@angular/common/http';
+// import {environment} from '../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UsersService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UsersService {
 
-  constructor(private http:HttpClient) { }
+//   constructor(private http:HttpClient) { }
 
-  getUsers(){
-    return this.http.get(`https://api.github.com/users/carolwanzuu?access_token=${environment.githubApiKey}`)
-  }
-}
+//   getUsers(){
+//     return this.http.get(`https://api.github.com/users/carolwanzuu?access_token=${environment.githubApiKey}`)
+//   }
+// }
