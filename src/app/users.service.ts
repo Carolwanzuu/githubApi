541d7db74d9fd1f2bdcacf35c49b341596901907
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-24 05:29:04 +0300

    creating a http service using a promise

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,17 +1,47 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {environment} from '../environments/environment';
-//import {UsersClass} from './user/users-class';
+import {UsersClass} from './user/users-class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private http:HttpClient) { }
+  users:UsersClass;
+  usersClass: any;
 
-  getUsers(){
-    //return this.http.get(`https://api.github.com/users/carolwanzuu?access_token=${environment.githubApiKey}`)
+  constructor(private http:HttpClient) {
+    this.users = new UsersClass("","");
+   }
+
+   usersService(){
+     interface ApiResponse{
+       name:string;
+       description:string;
+     }
+     let promise = new Promise((resolve,reject)=>{
+       this.http.get<ApiResponse>("https://api.github.com/users/carolwanzuu?access_token=${environment.githubApiKey}").toPromise().then(response=>{
+          this.usersClass.name =response.name
+          this.usersClass.description = response.description
+
+          resolve("")
+       },
+       error=>{
+         this.usersClass.name = "Carol"
+         this.usersClass.description = "wrong destination"
+
+         reject(error)
+       }
+       )
+     })
+     return promise
+   }
+
+
+
+  // getUsers(){
+  //   return this.http.get(`https://api.github.com/users/carolwanzuu?access_token=${environment.githubApiKey}`)
   
-  }
+  // }
 }
