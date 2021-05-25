2f55426fc7850cd2aa3ef01dab8c54f87f24480b
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-25 05:31:21 +0300

    imported components in the app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {RepositoriesComponent} from './repositories/repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search', component:SearchComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
