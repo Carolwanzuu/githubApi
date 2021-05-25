de0520b8fb2b36e34294c3c8479a25f4b0c9fa18
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-25 05:39:47 +0300

    added profile component to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProfileComponent} from './profile/profile.component';
 import {SearchComponent} from './search/search.component';
 import {RepositoriesComponent} from './repositories/repositories.component';
 
 const routes: Routes = [
+  {path: 'profile', component:ProfileComponent},
   {path: 'search', component:SearchComponent},
   {path: 'repositories', component: RepositoriesComponent}
 ];
