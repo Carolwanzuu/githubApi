a6bdd1fe50922ab6e9e677e0e579f83845b53aa0
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-26 13:15:42 +0300

    generated a directive to highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MyReposComponent } from './my-repos/my-repos.component';
 import { SearchReposComponent } from './search-repos/search-repos.component';
 
 import { CapitalizePipe } from './capitalize.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CapitalizePipe } from './capitalize.pipe';
     NavbarComponent,
     MyReposComponent,
     SearchReposComponent,
-    CapitalizePipe
+    CapitalizePipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
